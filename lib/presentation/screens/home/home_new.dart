950b71baf9dab996a5b6553188fa0b20c869a9c7
shaaanuu <monshan50@gmail.com>
2025-01-09 19:46:24 +0530

    fix: null value error in the home2screen

--- lib/presentation/screens/home/home_new.dart
+++ lib/presentation/screens/home/home_new.dart
@@ -46,17 +46,24 @@ class HomeNew extends StatelessWidget {
       appBar: AppBar(
         title: const Text("For testing purposes..."),
       ),
-      body: FutureBuilder(
+      body: FutureBuilder<List<String>>(
         future: fetchDir(),
-        builder: (context, snapshot) => SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) => Text(
-              snapshot.data![index],
-            ),
-          ),
-        ),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No files or directories found.'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(snapshot.data![index]),
+              ),
+            );
+          }
+        },
       ),
     );
   }
