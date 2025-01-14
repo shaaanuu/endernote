3e135ce6a4b1d030d12803d5d7561574d6eb17b6
shaaanuu <monshan50@gmail.com>
2025-01-14 14:27:01 +0530

    update: created file and folder in the home directory

--- lib/presentation/screens/home/screen_home.dart
+++ lib/presentation/screens/home/screen_home.dart
@@ -3,10 +3,12 @@ import 'dart:io';
 import 'package:ficonsax/ficonsax.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 import '../../../bloc/directory/directory_bloc.dart';
 import '../../../bloc/directory/directory_events.dart';
 import '../../../bloc/directory/directory_states.dart';
+import '../../theme/endernote_theme.dart';
 
 class ScreenHome extends StatelessWidget {
   const ScreenHome({super.key, required this.rootPath});
@@ -15,12 +17,14 @@ class ScreenHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ValueNotifier<bool> isDialOpen = ValueNotifier(false);
+    TextEditingController folderController = TextEditingController();
+    TextEditingController fileController = TextEditingController();
+
     return BlocProvider(
       create: (_) => DirectoryBloc()..add(FetchDirectory(rootPath)),
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Endernote"),
-        ),
+        appBar: AppBar(title: const Text("Endernote")),
         body: BlocBuilder<DirectoryBloc, DirectoryState>(
           builder: (context, state) {
             if (state.isLoading) {
@@ -33,6 +37,152 @@ class ScreenHome extends StatelessWidget {
 
             return _buildDirectoryList(context, rootPath, state);
           },
+        ),
+        floatingActionButton: SpeedDial(
+          openCloseDial: isDialOpen,
+          children: [
+            SpeedDialChild(
+              child: const Icon(IconsaxOutline.folder),
+              label: "Folder",
+              onTap: () => showDialog(
+                context: context,
+                builder: (context) => Dialog(
+                  child: Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      color: clrBase,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            "New Folder",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: folderController,
+                            decoration: const InputDecoration(
+                              hintText: 'Folder name',
+                              hintStyle: TextStyle(
+                                color: clrText,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              TextButton(
+                                child: const Text("Cancel"),
+                                onPressed: () {
+                                  folderController.clear();
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              TextButton(
+                                child: const Text("Create"),
+                                onPressed: () async {
+                                  if (folderController.text != "") {
+                                    await Directory(
+                                      '$rootPath/${folderController.text}',
+                                    ).create(recursive: true);
+                                  }
+                                  folderController.clear();
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SpeedDialChild(
+              child: const Icon(IconsaxOutline.task_square),
+              label: "Note",
+              onTap: () => showDialog(
+                context: context,
+                builder: (context) => Dialog(
+                  child: Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      color: clrBase,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            "New File",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: fileController,
+                            decoration: const InputDecoration(
+                              hintText: 'File name',
+                              hintStyle: TextStyle(
+                                color: clrText,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              TextButton(
+                                child: const Text("Cancel"),
+                                onPressed: () {
+                                  fileController.clear();
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              TextButton(
+                                child: const Text("Create"),
+                                onPressed: () async {
+                                  if (fileController.text != "") {
+                                    await File(
+                                      '$rootPath/${fileController.text}',
+                                    ).create(recursive: true);
+                                  }
+                                  fileController.clear();
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+          child: const Icon(IconsaxOutline.add),
         ),
       ),
     );
