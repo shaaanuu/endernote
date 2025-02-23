a4cb014217a1069e321dc4cf38abd91b4ba2407b
shaaanuu <monshan50@gmail.com>
2025-02-23 21:52:17 +0530

    fix: overflow of buttons in heropage

--- lib/presentation/screens/hero/screen_hero.dart
+++ lib/presentation/screens/hero/screen_hero.dart
@@ -74,125 +74,131 @@ class ScreenHero extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 300,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Untangle',
-                      style: TextStyle(
-                        fontFamily: 'Barriecito',
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFFf2cdcd),
-                        height: 1.1,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 12.0, top: 10),
-                          child: SvgPicture.asset(
-                            'lib/assets/brain.svg',
-                            width: 70,
-                            height: 70,
-                            colorFilter: const ColorFilter.mode(
-                              Color(0xFFf38ba8),
-                              BlendMode.srcIn,
-                            ),
-                          ),
-                        ),
-                        const Text(
-                          'Your',
-                          style: TextStyle(
-                            fontFamily: 'Barriecito',
-                            fontSize: 60,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFFb4befe),
-                            height: 1.1,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Text(
-                      'Thoughts',
-                      style: TextStyle(
-                        fontFamily: 'Barriecito',
-                        fontSize: 60,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFFcba6f7),
-                        height: 1.1,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 60),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 300,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  ElevatedButton.icon(
-                    icon: const Icon(
-                      IconsaxOutline.note_2,
-                      size: 24,
-                      color: Color(0xFF1e1e2e),
+                  const Text(
+                    'Untangle',
+                    style: TextStyle(
+                      fontFamily: 'Barriecito',
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFf2cdcd),
+                      height: 1.1,
                     ),
-                    label: const Text(
-                      'Create new note',
-                      style: TextStyle(
-                        color: Color(0xFF1e1e2e),
-                        fontSize: 16,
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF89b4fa),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10,
-                        vertical: 16,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25),
-                      ),
-                    ),
-                    onPressed: () async {
-                      final newFile = File(
-                        '$rootPath/new_note_${DateTime.now().millisecondsSinceEpoch}.md',
-                      );
-                      await newFile.create();
-
-                      Navigator.pushNamed(
-                        context,
-                        '/canvas',
-                        arguments: newFile.path,
-                      );
-                    },
                   ),
-                  const SizedBox(width: 16),
-                  OutlinedButton.icon(
-                    icon: const Icon(IconsaxOutline.folder),
-                    label: const Text('Open a note'),
-                    style: OutlinedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10,
-                        vertical: 16,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 12.0, top: 10),
+                        child: SvgPicture.asset(
+                          'lib/assets/brain.svg',
+                          width: 70,
+                          height: 70,
+                          colorFilter: const ColorFilter.mode(
+                            Color(0xFFf38ba8),
+                            BlendMode.srcIn,
+                          ),
+                        ),
                       ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25),
+                      const Text(
+                        'Your',
+                        style: TextStyle(
+                          fontFamily: 'Barriecito',
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFb4befe),
+                          height: 1.1,
+                        ),
                       ),
+                    ],
+                  ),
+                  const Text(
+                    'Thoughts',
+                    style: TextStyle(
+                      fontFamily: 'Barriecito',
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFcba6f7),
+                      height: 1.1,
                     ),
-                    onPressed: () => Navigator.pushNamed(context, '/home'),
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  icon: const Icon(
+                    IconsaxOutline.note_2,
+                    size: 24,
+                    color: Color(0xFF1e1e2e),
+                  ),
+                  label: const Text(
+                    'Create new note',
+                    style: TextStyle(
+                      fontFamily: 'FiraCode',
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF1e1e2e),
+                      wordSpacing: -3,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF89b4fa),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 16,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  onPressed: () async {
+                    final newFile = File(
+                      '$rootPath/new_note_${DateTime.now().millisecondsSinceEpoch}.md',
+                    );
+                    await newFile.create();
+        
+                    Navigator.pushNamed(
+                      context,
+                      '/canvas',
+                      arguments: newFile.path,
+                    );
+                  },
+                ),
+                const SizedBox(width: 16),
+                OutlinedButton.icon(
+                  icon: const Icon(IconsaxOutline.folder),
+                  label: const Text(
+                    'Open a note',
+                    style: TextStyle(
+                      fontFamily: 'FiraCode',
+                      fontWeight: FontWeight.bold,
+                      wordSpacing: -3,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 16,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  onPressed: () => Navigator.pushNamed(context, '/home'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
       floatingActionButton: CustomFAB(rootPath: rootPath),
