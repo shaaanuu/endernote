0682280d21b884e692d54210c71d9afecbb2ee7a
shaaanuu <monshan50@gmail.com>
2025-02-24 20:54:25 +0530

    add: floating toolbar in the canvas

--- lib/presentation/screens/canvas/edit_mode/edit_mode.dart
+++ lib/presentation/screens/canvas/edit_mode/edit_mode.dart
@@ -41,31 +41,135 @@ class EditMode extends StatelessWidget {
             },
           );
 
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: const EdgeInsets.all(5),
-              color: Colors.black12,
-              child: TextField(
-                decoration: InputDecoration(
-                  floatingLabelStyle: TextStyle(
-                    color:
-                        Theme.of(context).extension<EndernoteColors>()?.clrText,
+          return Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  padding: const EdgeInsets.all(5),
+                  color: Colors.black12,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      floatingLabelStyle: TextStyle(
+                        color: Theme.of(context)
+                            .extension<EndernoteColors>()
+                            ?.clrText,
+                      ),
+                      border: InputBorder.none,
+                      labelStyle: TextStyle(
+                        color: Theme.of(context)
+                            .extension<EndernoteColors>()
+                            ?.clrText,
+                      ),
+                      enabledBorder: InputBorder.none,
+                    ),
+                    style: const TextStyle(fontFamily: 'FiraCode'),
+                    controller: textController,
+                    expands: true,
+                    minLines: null,
+                    maxLines: null,
                   ),
-                  border: InputBorder.none,
-                  labelStyle: TextStyle(
-                    color:
-                        Theme.of(context).extension<EndernoteColors>()?.clrText,
-                  ),
-                  enabledBorder: InputBorder.none,
                 ),
-                style: const TextStyle(fontFamily: 'FiraCode'),
-                controller: textController,
-                expands: true,
-                minLines: null,
-                maxLines: null,
               ),
-            ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(12, 0, 0, 24),
+                  decoration: BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      topLeft: Radius.circular(20),
+                    ),
+                  ),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.format_bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.format_italic,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.format_underline,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.strikethrough_s,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.format_list_bulleted,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.format_list_numbered,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.code,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.link,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.image,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.format_quote,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.horizontal_rule,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           );
         }
       },
