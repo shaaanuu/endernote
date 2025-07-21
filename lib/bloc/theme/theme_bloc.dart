9164dfa2ff00829dd030f5d5abd90eceee230ae2
shaaanuu <monshan50@gmail.com>
2025-07-21 20:42:44 +0530

    migrate: flutterSecureStorage to SharedPreference

--- lib/bloc/theme/theme_bloc.dart
+++ lib/bloc/theme/theme_bloc.dart
@@ -1,36 +1,30 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../presentation/theme/app_themes.dart';
 import 'theme_events.dart';
 import 'theme_states.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
-  final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
-
   ThemeBloc() : super(ThemeState(AppTheme.catppuccinMocha)) {
-    _loadSavedTheme();
-
     on<ChangeThemeEvent>((event, emit) async {
+      final prefs = await SharedPreferences.getInstance();
       emit(ThemeState(event.theme));
-      await _secureStorage.write(
-        key: "selectedTheme",
-        value: event.theme.toString(),
-      );
+      prefs.setString("selectedTheme", event.theme.name);
     });
 
     on<LoadSavedThemeEvent>((event, emit) {
       emit(ThemeState(event.theme));
     });
+
+    _loadSavedTheme();
   }
 
   Future<void> _loadSavedTheme() async {
-    final savedTheme = await _secureStorage.read(key: "selectedTheme");
+    final prefs = await SharedPreferences.getInstance();
+    final savedTheme = prefs.getString("selectedTheme");
     if (savedTheme != null) {
-      final theme = AppTheme.values.firstWhere(
-        (theme) => theme.toString() == savedTheme,
-        orElse: () => AppTheme.catppuccinMocha,
-      );
+      final theme = AppTheme.values.byName(savedTheme);
       add(LoadSavedThemeEvent(theme));
     }
   }
