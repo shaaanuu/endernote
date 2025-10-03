b308528945627d0b6ee5b1df9bc796e1a22ce9f7
shaaanuu <monshan50@gmail.com>
2025-10-03 15:53:13 +0530

    optimize: refactored dialog box

---
+++ lib/presentation/widgets/custom_dialog.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import '../theme/app_themes.dart';
+
+class CustomDialog extends StatelessWidget {
+  const CustomDialog({
+    super.key,
+    required this.controller,
+    required this.icon,
+    required this.label,
+    required this.onCreate,
+  });
+
+  final TextEditingController controller;
+  final IconData icon;
+  final String label;
+  final Future<void> Function() onCreate;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      actionsPadding: EdgeInsets.all(16),
+      contentPadding: EdgeInsets.all(16),
+      backgroundColor: Theme.of(context).extension<EndernoteColors>()?.clrBase,
+      content: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).extension<EndernoteColors>()?.clrSecondary,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: TextField(
+          controller: controller,
+          autofocus: true,
+          decoration: InputDecoration(
+            hintText: '$label name',
+            hintStyle: TextStyle(
+              color: Theme.of(context)
+                  .extension<EndernoteColors>()
+                  ?.clrTextSecondary
+                  .withAlpha(179),
+            ),
+            suffixIcon: Icon(icon),
+            suffixIconColor: Theme.of(context)
+                .extension<EndernoteColors>()
+                ?.clrTextSecondary
+                .withAlpha(179),
+            border: InputBorder.none,
+            enabledBorder: InputBorder.none,
+            contentPadding: const EdgeInsets.all(16),
+          ),
+          onSubmitted: (value) => onCreate(),
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+            controller.clear();
+          },
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: onCreate,
+          child: const Text('Create'),
+        ),
+      ],
+    );
+  }
+}
--- lib/presentation/widgets/custom_fab.dart
+++ lib/presentation/widgets/custom_fab.dart
@@ -9,6 +9,7 @@ import '../../bloc/file/file_bloc.dart';
 import '../../bloc/file/file_events.dart';
 import '../../bloc/file/file_states.dart';
 import '../theme/app_themes.dart';
+import 'custom_dialog.dart';
 
 class CustomFAB extends StatelessWidget {
   const CustomFAB({super.key, required this.rootPath});
@@ -88,53 +89,11 @@ class CustomFAB extends StatelessWidget {
           Theme.of(context).extension<EndernoteColors>()?.clrSecondary,
       onTap: () => showDialog(
         context: context,
-        builder: (context) => AlertDialog(
-          actionsPadding: EdgeInsets.all(16),
-          contentPadding: EdgeInsets.all(16),
-          backgroundColor:
-              Theme.of(context).extension<EndernoteColors>()?.clrBase,
-          content: Container(
-            decoration: BoxDecoration(
-              color:
-                  Theme.of(context).extension<EndernoteColors>()?.clrSecondary,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: TextField(
-              controller: controller,
-              autofocus: true,
-              decoration: InputDecoration(
-                hintText: '$label name',
-                hintStyle: TextStyle(
-                  color: Theme.of(context)
-                      .extension<EndernoteColors>()
-                      ?.clrTextSecondary
-                      .withAlpha(179),
-                ),
-                suffixIcon: Icon(icon),
-                suffixIconColor: Theme.of(context)
-                    .extension<EndernoteColors>()
-                    ?.clrTextSecondary
-                    .withAlpha(179),
-                border: InputBorder.none,
-                enabledBorder: InputBorder.none,
-                contentPadding: const EdgeInsets.all(16),
-              ),
-              onSubmitted: (value) => onCreate(),
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-                controller.clear();
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: onCreate,
-              child: const Text('Create'),
-            ),
-          ],
+        builder: (context) => CustomDialog(
+          controller: controller,
+          icon: icon,
+          label: label,
+          onCreate: onCreate,
         ),
       ),
     );
