2258db6aceb241525022909dc20f1c398c223019
shaaanuu <monshan50@gmail.com>
2026-02-15 16:03:08 +0530

    migrate: flutter_markdown to flutter_makrdown_plus

--- lib/presentation/screens/canvas/preview_mode/preview_mode.dart
+++ lib/presentation/screens/canvas/preview_mode/preview_mode.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../theme/app_themes.dart';
